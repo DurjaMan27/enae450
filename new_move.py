9879caea7a80a5220baa81008d7102a10d3d2a2d
Varun Rao <31515224+DurjaMan27@users.noreply.github.com>
2025-05-16 17:52:56 -0400

    Add files via upload

--- new_move.py
+++ new_move.py
@@ -56,6 +56,7 @@ class move_path(Node):
       if self.avg_ranges(msg.ranges[range_index:range_index+15]) > self.avg_ranges(msg.ranges[index:index+15]):
         index = range_index
 
+
     if index > 180 and index <= 320:
       self.turn('right', (index + 10) * (math.pi / 180))
     elif index < 180 and index >= 40:
@@ -63,23 +64,63 @@ class move_path(Node):
       sleep(2.0)
 
   def avg_ranges(self, range):
+    CLEARANCE = 0.3
     num_numbers = 0
     total = 0
     for value in range:
-      if value:
+      if value and value > CLEARANCE:
         num_numbers += 1
         total += value
+      else:
+        return float('-inf')
 
     return num_numbers / total
 
-  def right_or_left(self, msg):
-    distance_left = self.avg_ranges(msg.ranges[80:100])
-    distance_right = self.avg_ranges(msg.ranges[260:280])
+  def check_triangle_and_turn(self, msg):
+    BUFFER_DIST = 0.4
+    FRONT_CONE_ANGLE = 45
 
-    if distance_left > distance_right * 1.5:
-      self.turn('left', math.pi / 2)
-    else:
-      self.turn('right', math.pi / 2)
+    left_cone = msg.ranges[0:FRONT_CONE_ANGLE]
+    right_cone = msg.ranges[360 - FRONT_CONE_ANGLE:360]
+
+    left_close = any(r < BUFFER_DIST and r > 0.05 for r in left_cone)
+    right_close = any(r < BUFFER_DIST and r > 0.05 for r in right_cone)
+
+    if left_close and not right_close:
+        self.curved_turn("right")
+        print("Curved turn RIGHT to avoid left obstacle")
+
+    elif right_close and not left_close:
+        self.curved_turn("left")
+        print("Curved turn LEFT to avoid right obstacle")
+
+    elif left_close and right_close:
+        left_avg = self.avg_ranges(left_cone)
+        right_avg = self.avg_ranges(right_cone)
+        if left_avg > right_avg:
+            self.curved_turn("left")
+        else:
+            self.curved_turn("right")
+        print("Curved turn to avoid both sides")
+
+  def curved_turn(self, direction):
+    twist = Twist()
+    twist.linear.x = 0.05  # slow forward motion
+
+    if direction == "left":
+        twist.angular.z = 0.5
+    elif direction == "right":
+        twist.angular.z = -0.5
+
+    self.publisher.publish(twist)
+    sleep(0.5)
+
+    # Optional stop after curve
+    stop_msg = Twist()
+    stop_msg.linear = self.stop()
+    stop_msg.angular = self.stop()
+    self.publisher.publish(stop_msg)
+    sleep(0.2)
 
   def follow_wall(self, msg):
     print("Basically just going straight")
@@ -92,8 +133,9 @@ class move_path(Node):
     stop_msg.linear = self.stop()
     stop_msg.angular = self.stop()
 
-    print("checking for open pathways")
+    print("checking for open pathways and adjusting if necessary")
     self.check_largest_open_angle(msg)
+    self.check_triangle_and_turn(msg)
 
     FRONT = 0.3
     if self.avg_ranges(msg.ranges[0:10] + msg.ranges[349:]) < FRONT:
